cb831d8cb76f8309c9cd67baaef8056be1d4d753
Gabriel Trujillo (Globant) <joaquin.trujillo+github@globant.com>
2024-08-22 00:00:48 -0500

    Basic scope exercise

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-// run `node index.js` in the terminal
+const a = 1;
 
-console.log(`Hello Node.js v${process.versions.node}!`);
+function myFunction1() {
+    let a = 'dog';
+    console.log(`Function 1 a: ${a}`);
+}
+
+const myFunction2 = () => {
+    console.log(`Function 2 a: ${a}`);
+    var a = 'cat';
+}
+
+console.log(`Outside a: ${a}`);
+myFunction1();
+myFunction2();
